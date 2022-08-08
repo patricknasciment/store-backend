011064516bfde235712cae297475c50f575650b1
patricknasciment <patrick_evan@outlook.com>
2022-08-08 20:39:39 -0300

    ValidationError & ExceptionHandler

--- src/main/java/com/patrick/store/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/patrick/store/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.patrick.store.service.exeptions.DataIntegrityException;
 import com.patrick.store.service.exeptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -33,4 +35,21 @@ public class ResourceExceptionHandler {
                 status.value(), errorMessage, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(error);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e,
+                                                       HttpServletRequest request){
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String errorMessage = status.getReasonPhrase();
+        ValidationError error = new ValidationError(Instant.now(),
+                status.value(), errorMessage, "Validation Error", request.getRequestURI());
+
+        for (FieldError x : e.getBindingResult().getFieldErrors()){
+            error.addError(x.getField(), x.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(error);
+    }
+
+
 }
